d189100c4ef79035ac1f153a65da2b69e071a6e1
Edwin Plauchu <j4nusx@yahoo.com>
2024-03-11 11:43:48 -0600

    yep

--- proto/cameras/src/main/java/com/immortalcrab/meh/Camera.java
+++ proto/cameras/src/main/java/com/immortalcrab/meh/Camera.java
@@ -1,5 +1,8 @@
 package com.immortalcrab.meh;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,4 +17,24 @@ public class Camera {
 	private String name;
 
 	private String uri;
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder().append(name).append(uri).toHashCode();
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+
+		if (obj instanceof Camera) {
+			final Camera other = (Camera) obj;
+
+			return new EqualsBuilder().append(name, other.name).append(uri, other.uri).isEquals();
+		}
+
+		return false;
+	}
 }
